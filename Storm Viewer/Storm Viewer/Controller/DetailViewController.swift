b26a209eed2f5bdcae4c343bac50a0bfd41cfb07
developpeur.olive as Oliver <92441827+developeroliver@users.noreply.github.com>
2024-04-26 16:58:03 +0200

    add animation + project done

--- Storm Viewer/Storm Viewer/Controller/DetailViewController.swift
+++ Storm Viewer/Storm Viewer/Controller/DetailViewController.swift
@@ -20,9 +20,23 @@ class DetailViewController: UIViewController {
         setup()
         layout()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+       super.viewWillAppear(animated)
+       navigationController?.hidesBarsOnTap = true
+        self.fullImageView.alpha = 0.0
+        animateImageAppearance()
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+       super.viewWillDisappear(animated)
+       navigationController?.hidesBarsOnTap = false
+    }
 }
 
 // MARK: - @objc Functions
+extension DetailViewController {
+    
+}
 
 // MARK: - Helpers
 extension DetailViewController {
@@ -41,8 +55,15 @@ extension DetailViewController {
         
         if let imageName = selectedImageName {
             fullImageView.image = UIImage(named: imageName)
+            
         }
         
         view.addSubview(fullImageView)
     }
+    
+    private func animateImageAppearance() {
+        UIView.animate(withDuration: 1.3) {
+                self.fullImageView.alpha = 1.0
+            }
+        }
 }
