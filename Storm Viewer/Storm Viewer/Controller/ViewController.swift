5b2c6f8ca4c5c76092ad36a01554e2943a99eef9
developpeur.olive as Oliver <92441827+developeroliver@users.noreply.github.com>
2024-04-26 16:00:12 +0200

    create table view + some refacto

--- Storm Viewer/Storm Viewer/Controller/ViewController.swift
+++ Storm Viewer/Storm Viewer/Controller/ViewController.swift
@@ -7,16 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     
     // MARK: - Properties
     var pictures = [String]()
+    let reuseID = "CELL_ID"
 
     //MARK: - LifeCycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
         
+        setupFileManager()
+        setupTableView()
+        layout()
+    }
+}
+
+// MARK: - Helpers
+extension ViewController {
+    
+    private func setupFileManager() {
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         
@@ -25,9 +35,40 @@ class ViewController: UIViewController {
         for item in items {
             if item.hasPrefix("nssl") {
                 pictures.append(item)
-                print(pictures)
             }
         }
     }
+    
+    private func layout() {
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func setupTableView() {
+        tableView = UITableView(frame: view.bounds, style: .plain)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseID)
+        tableView.separatorColor = UIColor.blue.withAlphaComponent(0.1)
+        self.tableView = tableView
+    }
 }
 
+// MARK: - DataSource and Delegate
+extension ViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseID, for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        cell.accessoryType = .disclosureIndicator
+        
+        let selectedView = UIView()
+        selectedView.backgroundColor = UIColor.blue.withAlphaComponent(0.1)
+        cell.selectedBackgroundView = selectedView
+        
+        return cell
+    }
+}
